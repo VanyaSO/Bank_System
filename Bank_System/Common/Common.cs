e3fe533d6dc3cef931c17060f8b91ab80498ecae
Ivan Ushachov <ushachovg324@gmail.com>
2024-07-01 11:06:15 +0300

    created common class

---
+++ Bank_System/Common/Common.cs
@@ -0,0 +1,16 @@
+namespace Bank_System;
+
+public static class Common
+{
+    public static int GetAction(int maxVarAction, int minVarAction) {
+        int action;
+        
+        if (!Int32.TryParse(Console.ReadLine(), out action))
+            throw new Exception("Введённое значение не является числом");
+
+        if (action < minVarAction || action > maxVarAction)
+            throw new Exception("Введённое число выходит за пределы допустимого диапазона");
+
+        return action;
+    }
+}
