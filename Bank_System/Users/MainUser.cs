9bb14da7974446cd0cbef58f6263dd4ad5cf20f3
Max Teplov <153938230+43inviz@users.noreply.github.com>
2024-07-03 13:44:04 +0300

    add MainUser,BankUser + Admin

---
+++ Bank_System/Users/MainUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_System
+{
+    internal abstract class MainUser
+    {
+        public string Name { get; set; } //не меняется 
+        public string Login { get; set; } //не меняется
+        public string Password { get; set; }
+
+        public MainUser() { }
+        public MainUser(string name,string login, string password)
+        {
+            Name = name;
+            Login = login;
+            Password = password;
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return $"Имя: {Name}\nЛогин: {Login}\nПароль: {Password}";
+        }
+    }
+}
