3940028a2c78d700f68de4d5714ae0db2618b516
Ivan Ushachov <ushachov.ivan@armar.solutions>
2024-07-05 09:31:21 +0300

    feat: remove default text in Messages

--- Bank_System/Common/Message.cs
+++ Bank_System/Common/Message.cs
@@ -5,21 +5,21 @@ public static class Message
     public static void ErrorMessage(string mess)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Error: {mess}");
+        Console.WriteLine(mess);
         Console.ResetColor();
     }
     
     public static void WarningMessage(string mess)
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine($"Warning: {mess}");
+        Console.WriteLine(mess);
         Console.ResetColor();
     }
     
     public static void SuccessMessage(string mess)
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Success: {mess}");
+        Console.WriteLine(mess);
         Console.ResetColor();
     }
 }
