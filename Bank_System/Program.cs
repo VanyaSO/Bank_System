80ddfec24cb35afa5328ab69a19de26e2403f80c
Ivan Ushachov <ushachov.ivan@armar.solutions>
2024-07-05 09:32:14 +0300

    feat: dev. updated

--- Bank_System/Program.cs
+++ Bank_System/Program.cs
@@ -4,18 +4,22 @@ class Program
 {
     static void Main()
     {
-        //MainMenu.Menu();
-        Card card = new Card("1234", CurrencyType.UAH);
-        Card card1 = new Card("1234", CurrencyType.UAH);
-        card.Deposit(1500);
-        card.Withdraw(500);
-        card.BlockCard();
-        card.UnblockCard();
-        card.Transfer(card1, 500, CurrencyType.UAH, "Alex");
-        Console.WriteLine(card.ToString());
-        card.SetPinCode("4321");
-        card.VerifyPinCode("3432");
-        card.VerifyPinCode("4321");
+        Common.Bank = new Bank("MonoBank", 2.0, 2.0);
+        Console.ReadKey();
+        // MainMenu.Menu();
+        // Card card = new Card("1234", CurrencyType.UAH);
+        // Card card1 = new Card("1234", CurrencyType.UAH);
+        // card.Deposit(1500);
+        // card.Withdraw(500);
+        // card.BlockCard();
+        // card.UnblockCard();
+        // card.Transfer(card1, 500, CurrencyType.UAH, "Alex");
+        // Console.WriteLine(card.ToString());
+        // card.SetPinCode("4322");
+        // card.VerifyPinCode("3132");
+        // card.VerifyPinCode("1132");
+        // card.VerifyPinCode("3432");
+        // Console.WriteLine(card.ToString());
         // ivan text commit
     }
 }
